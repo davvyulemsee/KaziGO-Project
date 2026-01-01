109fd52ab101eb397db918a0da934cd2a8d7f08f
davvyulemsee <davgmaina@gmail.com>
2026-01-01 12:13:14 +0300

    Switch to Postgres on Railway using dj_database_url

--- kazigoproject/settings.py
+++ kazigoproject/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 import os.path
 from pathlib import Path
+import dj_database_url  # ← You'll need to install this
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -81,11 +82,20 @@ WSGI_APPLICATION = 'kazigoproject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        # Railway provides DATABASE_URL automatically when you reference the Postgres plugin
+        default='sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3'),
+        conn_max_age=600,
+        conn_health_checks=True,
+    )
 }
 
 
